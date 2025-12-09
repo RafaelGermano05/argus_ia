44aa849f86b32c2730257771f77eae2ee4c15602
RafaelGermano05 <rgermanovasconcelos@gmail.com>
2025-12-09 19:45:42 -0300

    fix/ template/analyze.html2

--- detection/views.py
+++ detection/views.py
@@ -25,7 +25,7 @@ from .models import AnalysisSession
 
 
 def analyze_page(request):
-    return render(request, 'templates/analyze.html')
+    return render(request, 'templates/detection/analyze.html')
 
 class DashboardView(View):
     def get(self, request):
